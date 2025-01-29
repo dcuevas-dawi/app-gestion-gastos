c946c55939bc7ea69a0327b9b17edbdd13398093
dcuevas-dawi <cuevasnaharro@cifpfbmoll.eu>
2025-01-29 17:41:48 +0100

    cambio de estilo al botón 'ir a la app' en dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,16 +7,11 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-                <a
-                    href="{{ url('/') }}"
-                    class="text-xl rounded-md p-6 text-blue-400 ring-1 ring-transparent transition hover:text-blue-700/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                >
-                    Ir a la App
-                </a>
+            <div class="flex flex-row justify-evenly items-center bg-white p-6 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+
+                {{ __("¡Estás loguead@!") }}
+                <a href="{{ url('/') }}" class="h-full bg-blue-500 text-white px-4 py-2 rounded">Ir a la App</a>
+
             </div>
         </div>
     </div>
